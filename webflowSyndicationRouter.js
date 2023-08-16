2974508a447a80e2dda19008330d4443c9491d58
rileyrichter <ben@webflow.com>
2023-08-15 21:38:17 -0500

    Updated function to live publish

--- webflowSyndicationRouter.js
+++ webflowSyndicationRouter.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const Webflow = require("webflow-api");
+const Bottleneck = require("bottleneck");
 require("dotenv").config();
 const router = express.Router();
+
+const limiter = new Bottleneck({
+  minTime: 1000,
+});
 
 // Array of sites with siteId, collectionId, and siteApiKey
 const sites = [
@@ -24,46 +29,55 @@ const sites = [
 ];
 
 router.post("/syndicate-post", async (req, res) => {
+  console.log("Webhook received");
+  const postData = req.body;
+
+  const tasks = sites.map((site) => {
+    console.log(site.siteId, site.collectionId, site.siteApiKey);
+    const postBodyHtml = convertToHtml(postData["post-body"]);
+    const newItem = {
+      _archived: false,
+      _draft: false,
+      name: postData.name,
+      slug: postData.slug,
+      "post-summary": postData["post-summary"],
+      "post-body": postBodyHtml,
+      featured: postData.featured,
+      "thumbnail-image": postData["thumbnail-image"].url,
+      "main-image": postData["main-image"].url,
+      color: postData.color,
+    };
+
+    const siteWebflow = new Webflow({
+      token: site.siteApiKey,
+    });
+
+    return limiter
+      .schedule(() =>
+        siteWebflow.post(`collections/${site.collectionId}/items?live=true`, {
+          fields: newItem,
+        })
+      )
+      .then((response) => {
+        console.log(
+          `Created post in site with ID: ${site.siteId} and collection ID: ${site.collectionId}`
+        );
+        return response;
+      })
+      .catch((error) => {
+        console.error(
+          `Failed to add to Webflow CMS for site with ID: ${site.siteId} and collection ID: ${site.collectionId}`,
+          error.response.data
+        );
+        throw error; // This will reject the Promise returned by tasks.
+      });
+  });
+
   try {
-    console.log("Webhook received");
-    const postData = req.body;
-
-    // For each site, create the same post in the respective Webflow collection
-    for (const site of sites) {
-      console.log(site.siteId, site.collectionId, site.siteApiKey);
-      const postBodyHtml = convertToHtml(postData["post-body"]);
-      const newItem = {
-        _archived: false,
-        _draft: false,
-        name: postData.name,
-        slug: postData.slug,
-        "post-summary": postData["post-summary"],
-        "post-body": postBodyHtml,
-        featured: postData.featured,
-        "thumbnail-image": postData["thumbnail-image"].url,
-        "main-image": postData["main-image"].url,
-        color: postData.color,
-      };
-
-      // Initialize Webflow API client for each site using its specific API key
-      const siteWebflow = new Webflow({
-        token: site.siteApiKey,
-      });
-
-      await siteWebflow.createItem({
-        collectionId: site.collectionId,
-        fields: newItem,
-      });
-
-      console.log(
-        `Created post in site with ID: ${site.siteId} and collection ID: ${site.collectionId}`
-      );
-    }
-
+    await Promise.all(tasks);
     res.status(200).send("Post syndicated across sites");
   } catch (error) {
-    console.error("Error in /syndicate-post: ", error);
-    res.status(500).send("Internal server error");
+    res.status(500).send("Failed to syndicate post across all sites");
   }
 });
 
